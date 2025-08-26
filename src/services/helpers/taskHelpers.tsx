1baa60758f3a307963eef3094845ab0535ce3deb
Guilherme Xavier <guilhermexavier339@gmail.com>
2025-08-26 09:56:42 -0300

    fix: remove export from const dayInMilliseond

--- src/services/helpers/taskHelpers.tsx
+++ src/services/helpers/taskHelpers.tsx
@@ -1,8 +1,8 @@
+const dayInMilliseconds = 24 * 60 * 60 * 1000;
+
 export const idGenerator = (): string => {
   return Math.floor(Math.random() * 1000000000000000000000).toString();
 };
-
-export const dayInMilliseconds = 24 * 60 * 60 * 1000;
 
 const getDateObject = (date: string): Date => {
   const [day, month, year] = date.split("/");
